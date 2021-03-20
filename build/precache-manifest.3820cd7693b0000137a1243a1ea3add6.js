e268bc1f7e702f6a1f2af90b9a8eaf78c87b8996
zzunebye <bhang96@gmail.com>
2021-03-20 16:14:01 +0800

    heroku

---
+++ build/precache-manifest.3820cd7693b0000137a1243a1ea3add6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f532fdba563ef89bad70d8359965614",
+    "url": "/breaking_bad_react/index.html"
+  },
+  {
+    "revision": "6a4c87f5dcd58ffce2e0",
+    "url": "/breaking_bad_react/static/css/main.a7faed51.chunk.css"
+  },
+  {
+    "revision": "49ce944e8fdd8db74510",
+    "url": "/breaking_bad_react/static/js/2.9f657e87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking_bad_react/static/js/2.9f657e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a4c87f5dcd58ffce2e0",
+    "url": "/breaking_bad_react/static/js/main.36aac8bd.chunk.js"
+  },
+  {
+    "revision": "2bf6ab170937a6446282",
+    "url": "/breaking_bad_react/static/js/runtime-main.827f6753.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking_bad_react/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking_bad_react/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking_bad_react/static/media/spinner.235b9b3e.gif"
+  }
+]);
